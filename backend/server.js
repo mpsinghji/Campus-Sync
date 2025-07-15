67046e8a66b101ba7d2d56f1de6f0a9839b0bab6
Manpreet Singh <Manpreet.singhcomet@gmail.com>
2025-07-15 10:46:32 +0530

    trying production

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,12 @@ import app from "./app.js";
 import connectdb from "./config/db.js";
 import dotenv from "dotenv";
 
-// Load environment variables
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
-// Connect to database
 connectdb();
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);
